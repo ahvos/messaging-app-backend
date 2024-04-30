b499d5bc151118c7ad0edf3e5f49a2dcc596efc2
ahvo <ashley.vo1210@gmail.com>
2024-04-29 23:48:25 -0600

    server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from 'express'
 import mongoose from 'mongoose'
 
+import Messages from './dbMessages.js'
+
+
 
 /* app configuration */
 const app = express()
@@ -20,6 +23,26 @@ mongoose.connect(connection_url, {
 
 /* api endpoints */
 app.get("/", (req, res) => res.status(200).send("Hello TheWebDev"))
+
+app.post('/messages/new', (req, res) => {
+    const dbMessage = req.body
+    Messages.create(dbMessage, (err, data) => {
+        if(err)
+            res.status(500).send(err)
+        else
+            res.status(201).send(data)
+    })
+})
+
+app.get('/messages/sync', (req, res) => {
+    Messages.find((err, data) => {
+        if(err) {
+            res.status(500).send(err)
+        } else {
+        res.status(200).send(data)
+        }
+    })
+})
 
 
 /* listener */
