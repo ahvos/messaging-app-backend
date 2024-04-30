947b6d41c546951bdda867030f546b2feb38a182
ahvo <ashley.vo1210@gmail.com>
2024-04-30 01:03:24 -0600

    mongoose connection

--- server.js
+++ server.js
@@ -31,29 +31,28 @@ mongoose.connect(connection_url)
 
 
 /* api endpoints */
-app.get("/", (req, res) => res.status(200).send("Hello TheWebDev"))
-
-/*
-app.post('/messages/new', (req, res) => {
-    const dbMessage = req.body
-    Messages.create(dbMessage, (err, data) => {
-        if(err)
-            res.status(500).send(err)
-        else
-            res.status(201).send(data)
-    })
-})
-
-app.get('/messages/sync', (req, res) => {
-    Messages.find((err, data) => {
-        if(err) {
-            res.status(500).send(err)
+const db = mongoose.connection
+db.once("open", () => {
+    console.log("DB Connected")
+    const msgCollection = db.collection("messagingmessages")
+    const changeStream = msgCollection.watch()
+    changeStream.on('change', change => {
+        console.log(change)
+        if (change.operationType === "insert") {
+            const messageDetails = change.fullDocument
+            pusher.trigger("messages", "inserted", {
+                name: messageDetails.name,
+                message: messageDetails.message,
+                timestamp: messageDetails.timestamp,
+                received: messageDetails.received
+            })
         } else {
-        res.status(200).send(data)
+            console.log('Error trigerring Pusher')
         }
     })
 })
-*/
+
+app.get("/", (req, res) => res.status(200).send("Hello TheWebDev"))
 
 app.post('/messages/new', async (req, res) => {
     try {
